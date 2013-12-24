f94cdc5bd967571875ff39a19b6e7759848fdf6e
Eric <Ericrius1@gmail.com>
2013-12-23 16:28:25 -0800

    render depth

--- build/js/tree.js
+++ build/js/tree.js
@@ -9,9 +9,9 @@
     function Tree(pos) {
       var position, y, _i, _ref;
       this.position = pos;
-      this.treeTick = 2;
+      this.treeTick = 5;
       this.ornamentGroups = [];
-      this.ornamentTick = .16;
+      this.ornamentTick = .08;
       this.numLayers = 100;
       this.treeGroup = new ShaderParticleGroup({
         texture: THREE.ImageUtils.loadTexture('assets/leaf2.png'),
@@ -21,7 +21,7 @@
       for (y = _i = 1, _ref = this.numLayers; 1 <= _ref ? _i <= _ref : _i >= _ref; y = 1 <= _ref ? ++_i : --_i) {
         position = new THREE.Vector3(rnd(this.position.x - 10, this.position.x + 10), y * 4, this.position.z);
         this.treeGroup.addEmitter(this.generateTree(y, position));
-        this.createOrnamentGroup(y, position);
+        this.createOrnamentGroup(y);
       }
       FW.scene.add(this.treeGroup.mesh);
     }
@@ -32,7 +32,7 @@
       return treeEmitter = new ShaderParticleEmitter({
         size: 150,
         position: new THREE.Vector3(this.position.x, y * 4, this.position.z),
-        positionSpread: new THREE.Vector3(spread * rnd(0.9, 1), 10, spread * rnd(0.9, 1)),
+        positionSpread: new THREE.Vector3(spread, 10, spread),
         colorEnd: new THREE.Color(),
         particlesPerSecond: 10 / y,
         opacityEnd: 1.0
@@ -43,7 +43,7 @@
       var ornamentGroup, _i, _len, _ref, _results;
       this.treeGroup.tick(this.treeTick);
       if (this.treeTick > 0.0) {
-        this.treeTick -= .01;
+        this.treeTick -= .4;
       }
       _ref = this.ornamentGroups;
       _results = [];
@@ -58,54 +58,40 @@
       var ornamentGroup;
       ornamentGroup = new ShaderParticleGroup({
         texture: THREE.ImageUtils.loadTexture('assets/star.png'),
-        maxAge: 10,
-        blending: THREE.NormalBlending
+        maxAge: 20,
+        blending: THREE.AdditiveBlending
       });
-      ornamentGroup.addEmitter(this.generateOrnaments(y, position));
+      ornamentGroup.addEmitter(this.generateOrnaments(y));
       this.ornamentGroups.push(ornamentGroup);
-      return FW.scene.add(ornamentGroup.mesh);
+      FW.scene.add(ornamentGroup.mesh);
+      return ornamentGroup.mesh.renderDepth = -1;
     };
 
     Tree.prototype.generateOrnaments = function(y) {
       var colorStart, ornamentEmmiter, spread;
       spread = Math.max(0, 250 - y * 2.5);
       colorStart = new THREE.Color();
-      colorStart.setRGB(0x8d1818);
+      if (Math.random() < .5) {
+        colorStart.setHex(0xff0000);
+      } else {
+        colorStart.setHex(0xff00ff);
+      }
       return ornamentEmmiter = new ShaderParticleEmitter({
         size: 200,
         sizeEnd: 0,
-        colorEnd: new THREE.Color(0xff0000),
+        colorStart: new THREE.Color('white'),
+        colorEnd: colorStart,
         position: new THREE.Vector3(this.position.x, y * 4, this.position.z),
-        positionSpread: new THREE.Vector3(spread * rnd(0.9, 1), 10, spread),
-        particlesPerSecond: .1,
+        positionSpread: new THREE.Vector3(spread + 10, 10, spread + 10),
+        particlesPerSecond: 1,
         opacityStart: 0.5,
-        opacityMiddle: 1,
-        opacityEnd: 0.5
+        opacityMiddle: 1.0,
+        opacityEnd: 1.0
       });
     };
 
     return Tree;
 
   })();
-
-  ({
-    generateOrnaments2: function(y) {
-      var colorStart, ornamentEmmiter, spread;
-      spread = Math.max(0, 250 - y * 2.5);
-      colorStart = new THREE.Color();
-      colorStart.setRGB(0x8d1818);
-      return ornamentEmmiter = new ShaderParticleEmitter({
-        size: 200,
-        sizeEnd: 0,
-        colorEnd: new THREE.Color(0xff00ff),
-        position: new THREE.Vector3(this.position.x, y * 4, this.position.z),
-        positionSpread: new THREE.Vector3(spread * rnd(0.9, 1), 10, spread),
-        particlesPerSecond: .1,
-        opacityStart: 0.5,
-        opacityMiddle: 1,
-        opacityEnd: 0.5
-      });
-    }
-  });
 
 }).call(this);
--- src/tree.coffee
+++ src/tree.coffee
@@ -2,9 +2,9 @@ FW.Tree = class Tree
   rnd = FW.rnd
   constructor: (pos)->
     @position = pos
-    @treeTick = 2
+    @treeTick = 5
     @ornamentGroups = []
-    @ornamentTick = .16
+    @ornamentTick = .08
     @numLayers = 100
     @treeGroup = new ShaderParticleGroup({
       texture: THREE.ImageUtils.loadTexture('assets/leaf2.png')
@@ -15,8 +15,9 @@ FW.Tree = class Tree
     for y in [1..@numLayers]
       position = new THREE.Vector3 rnd(@position.x-10, @position.x+10), y*4,  @position.z
       @treeGroup.addEmitter @generateTree(y, position)
-      @createOrnamentGroup(y, position)
+      @createOrnamentGroup(y)
     FW.scene.add(@treeGroup.mesh)
+
 
   generateTree: (y)->
     spread = Math.max 0, 250 - y* 2.5
@@ -24,17 +25,19 @@ FW.Tree = class Tree
       size: 150
       position: new THREE.Vector3 @position.x,  y*4, @position.z
       #As we go higher, we want spread less to give xmas tree pyramid shape
-      positionSpread: new THREE.Vector3 spread * rnd(0.9, 1) , 10, spread * rnd(0.9, 1)
+      positionSpread: new THREE.Vector3 spread , 10, spread
       colorEnd: new THREE.Color()
       particlesPerSecond: 10/y
       opacityEnd: 1.0
   
-    
+  ##########################################
+  #TICK MAIN LOOOOP
+
   tick: ->
     # @treeGroup.tick(FW.globalTick/2)
     @treeGroup.tick(@treeTick)
     if @treeTick > 0.0
-      @treeTick -=.01 
+      @treeTick -=.4 
     
     for ornamentGroup in @ornamentGroups
       ornamentGroup.tick(@ornamentTick)
@@ -43,43 +46,34 @@ FW.Tree = class Tree
   createOrnamentGroup: (y, position)->
       ornamentGroup = new ShaderParticleGroup(
         texture: THREE.ImageUtils.loadTexture('assets/star.png')
-        maxAge: 10
-        blending: THREE.NormalBlending
+        maxAge: 20
+        blending: THREE.AdditiveBlending
       )
 
-      ornamentGroup.addEmitter @generateOrnaments(y, position)
+      ornamentGroup.addEmitter @generateOrnaments(y)
       @ornamentGroups.push ornamentGroup
       FW.scene.add ornamentGroup.mesh
+      ornamentGroup.mesh.renderDepth = -1
 
 
   generateOrnaments: (y)->
     spread = Math.max 0, 250 - y * 2.5
     colorStart = new THREE.Color()
-    colorStart.setRGB(0x8d1818)
+    if Math.random() < .5
+      colorStart.setHex(0xff0000)
+    else
+      colorStart.setHex(0xff00ff)
     ornamentEmmiter = new ShaderParticleEmitter
       size: 200
       sizeEnd: 0
-      colorEnd: new THREE.Color(0xff0000)
+      colorStart: new THREE.Color('white')
+      colorEnd: colorStart
       position: new THREE.Vector3 @position.x, y*4, @position.z
-      positionSpread: new THREE.Vector3 spread * rnd(0.9, 1), 10, spread
-      particlesPerSecond: .1
-      opacityStart: 0.5
-      opacityMiddle: 1
-      opacityEnd: 0.5
+      positionSpread: new THREE.Vector3 spread+ 10, 10, spread+ 10
+      particlesPerSecond: 1
+      opacityStart: 0.5 
+      opacityMiddle: 1.0
+      opacityEnd: 1.0
 
-generateOrnaments2: (y)->
-  spread = Math.max 0, 250 - y * 2.5
-  colorStart = new THREE.Color()
-  colorStart.setRGB(0x8d1818)
-  ornamentEmmiter = new ShaderParticleEmitter
-    size: 200
-    sizeEnd: 0
-    colorEnd: new THREE.Color(0xff00ff)
-    position: new THREE.Vector3 @position.x, y*4, @position.z
-    positionSpread: new THREE.Vector3 spread * rnd(0.9, 1), 10, spread
-    particlesPerSecond: .1
-    opacityStart: 0.5
-    opacityMiddle: 1
-    opacityEnd: 0.5
 
 
